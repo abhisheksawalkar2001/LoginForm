ff78a4df6a91d61ef1a3c0ccb100cb69b41f00ff
abhisheksawalkar2001 <abhisheksawalkar2001@gmail.com>
2021-09-05 23:23:38 +0530

    project

---
+++ accounts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
+# # Create your models here.
+
+
+# class User(AbstractUser):
+#     username = None
+#     email = models.CharField(max_length=50, unique=True)
+#     password = models.CharField(max_length=50)
+#     address = models.CharField(max_length=50)
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = []
